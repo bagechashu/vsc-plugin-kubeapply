19278ba2db039be3f3fadd6d06d4ef72cbbd1dc5
lixiaoxue <lixiaoxue@example.com>
2022-05-13 16:58:07 +0400

    [update] add diff command

--- README.md
+++ README.md
@@ -7,13 +7,18 @@ To apply a yaml file you can:
 - right click on the file + `K8S: Apply resource`
 - run the `K8S: Apply resource` command
 
+To get diff from yaml file you can:
+- right click on the file + `K8S: Diff resource`
+- run the `K8S: Diff resource` command
+
 To delete a yaml file you can:
 - right click on the file + `K8S: Delete resource`
 - run the `K8S: Delete resource` command
-
-
 
 ## Requirements
 
 - a configured `kubectl`
 - an active terminal in VSCode
+
+## Special Thanks List
+- [shaimendel](https://github.com/shaimendel/vscode-plugin-cicd-github-actions)
--- src/extension.ts
+++ src/extension.ts
@@ -1,53 +1,67 @@
-import * as vscode from 'vscode';
-import * as path from 'path';
+import * as vscode from "vscode";
+import * as path from "path";
 
 export function activate(context: vscode.ExtensionContext) {
-	console.log('Congratulations, your extension "kubernetesapply" is now active!');
+  console.log('Congratulations, your extension "kubeApply" is now active!');
 
-	function runResourceCommand(command: string, uri: vscode.Uri | undefined) {
-		if (uri === undefined && vscode.window.activeTextEditor === undefined) {
-			return;
-		}
+  function runResourceCommand(command: string, uri: vscode.Uri | undefined) {
+    if (uri === undefined && vscode.window.activeTextEditor === undefined) {
+      return;
+    }
 
-		let resourcePath = '';
-		if (uri !== undefined) {
-			resourcePath = uri.fsPath;
-		}
-		else if (vscode.window.activeTextEditor !== undefined) {
-			const currentlyOpenTabfilePath = vscode.window.activeTextEditor.document.fileName;
-			resourcePath = path.basename(currentlyOpenTabfilePath);
-		}
-	 
-		if (!ensureTerminalExists()) {
-			return;
-		}
+    let resourcePath = "";
+    if (uri !== undefined) {
+      resourcePath = uri.fsPath;
+    } else if (vscode.window.activeTextEditor !== undefined) {
+      const currentlyOpenTabfilePath =
+        vscode.window.activeTextEditor.document.fileName;
+      resourcePath = path.basename(currentlyOpenTabfilePath);
+    }
 
-		const terminal = selectTerminal();
-		terminal.sendText(`kubectl ${command} -f ${resourcePath}`);
-	}
+    if (!ensureTerminalExists()) {
+      return;
+    }
 
-	let disposableApply = vscode.commands.registerCommand('kubernetesapply.apply', (uri:vscode.Uri) => {
-		runResourceCommand('apply', uri);
-	});
+    const terminal = selectTerminal();
+    terminal.sendText(`kubectl ${command} -f ${resourcePath}`);
+  }
 
-	let disposableDelete = vscode.commands.registerCommand('kubernetesapply.delete', (uri:vscode.Uri) => {
-		runResourceCommand('delete', uri);
-	});
+  let disposableApply = vscode.commands.registerCommand(
+    "kubeApply.apply",
+    (uri: vscode.Uri) => {
+      runResourceCommand("apply", uri);
+    }
+  );
 
-	context.subscriptions.push(disposableApply);
-	context.subscriptions.push(disposableDelete);
+  let disposableDiff = vscode.commands.registerCommand(
+    "kubeApply.diff",
+    (uri: vscode.Uri) => {
+      runResourceCommand("diff", uri);
+    }
+  );
+
+  let disposableDelete = vscode.commands.registerCommand(
+    "kubeApply.delete",
+    (uri: vscode.Uri) => {
+      runResourceCommand("delete", uri);
+    }
+  );
+
+  context.subscriptions.push(disposableApply);
+  context.subscriptions.push(disposableDiff);
+  context.subscriptions.push(disposableDelete);
 }
 
 export function deactivate() {}
 
 function ensureTerminalExists(): boolean {
-	if ((<any>vscode.window).terminals.length === 0) {
-		vscode.window.showErrorMessage('No active terminals');
-		return false;
-	}
-	return true;
+  if ((<any>vscode.window).terminals.length === 0) {
+    vscode.window.showErrorMessage("No active terminals");
+    return false;
+  }
+  return true;
 }
 
 function selectTerminal(): vscode.Terminal {
-	return <vscode.Terminal>(<any>vscode.window.activeTerminal);
+  return <vscode.Terminal>(<any>vscode.window.activeTerminal);
 }
