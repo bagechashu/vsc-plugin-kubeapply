2327374c296050617b3e30cb3b13ac74e17f4d3c
lixiaoxue <lixiaoxue@example.com>
2023-01-27 17:40:09 +0400

    [update] change project name and command name

--- src/extension.ts
+++ src/extension.ts
@@ -37,8 +37,6 @@ export function activate(context: vscode.ExtensionContext) {
     const terminal = selectTerminal();
     if (!wait) {
       terminal.sendText(kubeApplyCmd);
-    } else if (command === "diff") {
-      terminal.sendText(kubeApplyCmd);
     } else if (command === "delete") {
       terminal.sendText(
         `${warnEcho}${endEcho} ${sp} ${wait5Min} ${sp} ${kubeApplyCmd}`
