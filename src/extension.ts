dc9cdb45a3cf68ed8c74e2010eaa72f421b9e823
lixiaoxue <lixiaoxue@example.com>
2023-01-27 10:42:01 +0400

    [update] add new button

--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,12 @@ import * as path from "path";
 export function activate(context: vscode.ExtensionContext) {
   console.log('Congratulations, your extension "kubeApply" is now active!');
 
-  function runResourceCommand(command: string, uri: vscode.Uri | undefined) {
+  function runResourceCommand(
+    wait: boolean,
+    command: string,
+    args: string,
+    uri: vscode.Uri | undefined
+  ) {
     if (uri === undefined && vscode.window.activeTextEditor === undefined) {
       return;
     }
@@ -27,48 +32,66 @@ export function activate(context: vscode.ExtensionContext) {
     let warnEcho: string = `echo -e "\n \\033[41m[ ${command.toUpperCase()} ]`;
     let endEcho: string = `\\033[0m will execute after 5s, \\033[;36;4m[Ctrl+c]\\033[0m to cancel"`;
     let wait5Min: string = `for i in $(seq 5); do  echo "." && sleep 1 ; done`;
-    let kubeApplyCmd: string = `kubectl ${command} -f ${resourcePath}`;
+    let kubeApplyCmd: string = `kubectl ${command} ${args} ${resourcePath}`;
 
     const terminal = selectTerminal();
-
-    if (command === "diff") {
+    if (!wait) {
       terminal.sendText(kubeApplyCmd);
-    } else if (command === "delete") {
-      terminal.sendText(
-        `${warnEcho}${endEcho} ${sp} ${wait5Min} ${sp} ${kubeApplyCmd}`
-      );
     } else {
       terminal.sendText(
         `${infoEcho}${endEcho} ${sp} ${wait5Min} ${sp} ${kubeApplyCmd}`
       );
     }
-
   }
 
   let disposableApply = vscode.commands.registerCommand(
     "kubeApply.apply",
     (uri: vscode.Uri) => {
-      runResourceCommand("apply", uri);
+      runResourceCommand(true, "apply", "-f", uri);
     }
   );
 
   let disposableDiff = vscode.commands.registerCommand(
     "kubeApply.diff",
     (uri: vscode.Uri) => {
-      runResourceCommand("diff", uri);
+      runResourceCommand(false, "diff", "-f", uri);
     }
   );
 
   let disposableDelete = vscode.commands.registerCommand(
     "kubeApply.delete",
     (uri: vscode.Uri) => {
-      runResourceCommand("delete", uri);
+      runResourceCommand(true, "delete", "-f", uri);
+    }
+  );
+
+  let disposableApplyNoWait = vscode.commands.registerCommand(
+    "kubeApply.apply-noWait",
+    (uri: vscode.Uri) => {
+      runResourceCommand(false, "apply", "-f", uri);
+    }
+  );
+
+  let disposableApplyKustomize = vscode.commands.registerCommand(
+    "kubeApply.apply-kustomize",
+    (uri: vscode.Uri) => {
+      runResourceCommand(true, "apply", "-k", uri);
+    }
+  );
+
+  let disposableDiffKustomize = vscode.commands.registerCommand(
+    "kubeApply.diff-kustomize",
+    (uri: vscode.Uri) => {
+      runResourceCommand(false, "diff", "-k", uri);
     }
   );
 
   context.subscriptions.push(disposableApply);
   context.subscriptions.push(disposableDiff);
   context.subscriptions.push(disposableDelete);
+  context.subscriptions.push(disposableApplyNoWait);
+  context.subscriptions.push(disposableApplyKustomize);
+  context.subscriptions.push(disposableDiffKustomize);
 }
 
 export function deactivate() {}
